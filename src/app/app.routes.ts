fd8c3345b52e839a644a9408835019ae1d888b2e
Diego Quipuzco <demential2003@gmail.com>
2025-04-27 12:32:48 -0500

    Se movieron las rutas para que todas necesiten que el usuario haya iniciado sesión.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,43 +4,47 @@ import { GameDetailsComponent } from './routing/game-details/game-details.compon
 import { CategoriesComponent } from './routing/categories/categories.component';
 import { CategoryPageComponent } from './routing/category-page/category-page.component';
 import { privateGuard } from './core/auth.guard';
+import { ApplicationLayoutComponent } from './components/application-layout/application-layout.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
     canActivate: [privateGuard()],
+    children: [
+      {
+        path: '',
+        component: ApplicationLayoutComponent,
+      },
+      {
+        path: 'categories',
+        component: CategoriesComponent,
+      },
+      {
+        path: 'categories/:id',
+        component: CategoryPageComponent,
+      },
+      {
+        path: 'game/:id',
+        component: GameDetailsComponent,
+      },
+      {
+        path: 'profile/:id',
+        loadComponent: () =>
+          import('./routing/profile/profile.component').then(
+            (m) => m.ProfileComponent
+          ),
+      },
+      {
+        path: 'library',
+        loadComponent: () =>
+          import('./routing/library/library.component').then(
+            (m) => m.LibraryComponent
+          ),
+      },
+    ],
   },
   {
     path: 'auth',
     loadChildren: () => import('./auth/features/auth.routes'),
-  },
-  {
-    path: 'categories',
-    component: CategoriesComponent,
-  },
-  {
-    path: 'categories/:id',
-    component: CategoryPageComponent,
-  },
-  {
-    path: 'game/:id',
-    component: GameDetailsComponent,
-  },
-  {
-    canActivate: [privateGuard()],
-    path: 'profile/:id',
-    loadComponent: () =>
-      import('./routing/profile/profile.component').then(
-        (m) => m.ProfileComponent
-      ),
-  },
-  {
-    canActivate: [privateGuard()],
-    path: 'library',
-    loadComponent: () =>
-      import('./routing/library/library.component').then(
-        (m) => m.LibraryComponent
-      ),
   },
 ];
