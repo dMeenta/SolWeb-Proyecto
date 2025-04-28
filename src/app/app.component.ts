0ba97163d33b2c9b1535cd0863809b413edc9ed8
Diego Quipuzco <demential2003@gmail.com>
2025-04-27 21:58:10 -0500

    Limpieza de archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,13 @@
-import { Component, inject, OnInit } from '@angular/core';
-import {
-  Router,
-  RouterLink,
-  RouterLinkActive,
-  RouterOutlet,
-} from '@angular/router';
-import { FriendsComponent } from './components/friends/friends.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { NgxSonnerToaster } from 'ngx-sonner';
-import { HomeButtonComponent } from './components/home-button/home-button.component';
-import { AuthStateService } from './shared/auth-state.service';
-import { NgClass } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    imports: [
-        RouterOutlet,
-        RouterLink,
-        FriendsComponent,
-        RouterLinkActive,
-        NgxSonnerToaster,
-        HomeButtonComponent,
-        NgClass,
-    ],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet, NgxSonnerToaster],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = '';
-  hiddenRoutes: string[] = ['/auth/sign-up', '/auth/sign-in'];
-  shouldHideLayout: boolean = false;
-  userConnected: boolean = false;
-  userId!: string;
-  private _authState = inject(AuthStateService);
-
-  constructor(private router: Router) {
-    this.router.events.subscribe(() => {
-      this.shouldHideLayout = this.hiddenRoutes.some((route) =>
-        this.router.url.startsWith(route)
-      );
-    });
-  }
-  ngOnInit(): void {
-    this._authState.authState$.subscribe((authUser) => {
-      if (authUser) {
-        this.userConnected = true;
-        this.userId = authUser.uid;
-      } else {
-        this.userConnected = false;
-      }
-    });
-  }
-
-  async salir() {
-    await this._authState.logOut();
-  }
 }
