a55481cb2fd71a104a361a5fee6275cc5de548e7
Diego Quipuzco <demential2003@gmail.com>
2025-04-29 23:20:14 -0500

    Condicional de Layout

--- src/app/components/application-layout/application-layout.component.ts
+++ src/app/components/application-layout/application-layout.component.ts
@@ -2,18 +2,21 @@ import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserMSQL } from '../../models/UserMSQL';
 import { AuthService } from '../../auth/data-access/auth.service';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { AsideMenuComponent } from './aside-menu/aside-menu.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-application-layout',
-  imports: [HeaderComponent, RouterOutlet, AsideMenuComponent],
+  imports: [HeaderComponent, RouterOutlet, AsideMenuComponent, NgClass],
   templateUrl: './application-layout.component.html',
   styleUrl: './application-layout.component.css',
 })
 export class ApplicationLayoutComponent implements OnInit {
   userLogged!: UserMSQL;
   private _authService = inject(AuthService);
+
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     const user = localStorage.getItem('userLogged');
@@ -25,5 +28,11 @@ export class ApplicationLayoutComponent implements OnInit {
 
   async logout(uid: string) {
     this._authService.logout(uid);
+  }
+
+  isGamePage() {
+    const url = this.router.url;
+    const segments = url.split('/').filter((seg) => seg);
+    return segments[0] === 'game';
   }
 }
