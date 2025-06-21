2f41ec80737c33a361984280d8cda03cfe781f95
Diego Quipuzco <demential2003@gmail.com>
2025-06-20 22:06:31 -0500

    getInfo with Cookies

--- src/app/components/application-layout/application-layout.component.ts
+++ src/app/components/application-layout/application-layout.component.ts
@@ -1,11 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { UserMSQL } from '../../models/UserMSQL';
-import { AuthService } from '../../auth/data-access/auth.service';
 import { Router, RouterOutlet } from '@angular/router';
 import { AsideMenuComponent } from './aside-menu/aside-menu.component';
 import { NgClass } from '@angular/common';
-import { SharedService } from '../../shared/shared.service';
+import { MinimalUserInfo, SharedService } from '../../shared/shared.service';
 
 @Component({
   selector: 'app-application-layout',
@@ -14,27 +12,33 @@ import { SharedService } from '../../shared/shared.service';
   styleUrl: './application-layout.component.css',
 })
 export class ApplicationLayoutComponent implements OnInit {
-  userLogged!: UserMSQL;
-  private _authService = inject(AuthService);
+  userLogged!: MinimalUserInfo;
   private _sharedService = inject(SharedService);
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {
-    const user = this._sharedService.getUserLogged();
-    if (user) {
-      this.userLogged = user;
-      console.log(user);
-    }
+    this.getUserLoggedInfo();
   }
 
-  async logout(uid: string) {
-    this._authService.logout(uid);
+  async logout() {
+    console.log('u log out');
   }
 
   isPageWidthFull() {
     const url = this.router.url;
     const segments = url.split('/').filter((seg) => seg);
     return segments[0] === 'game' || segments[0] === 'community';
+  }
+
+  getUserLoggedInfo() {
+    this._sharedService.getUserLogged().subscribe({
+      next: (res) => {
+        if (!res.success) {
+          console.error(res.message);
+        }
+        this.userLogged = res.data;
+      },
+    });
   }
 }
