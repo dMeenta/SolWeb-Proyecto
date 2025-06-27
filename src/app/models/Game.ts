f292d651ee89dee506271000bad63eb40bd3b38e
Diego Quipuzco <demential2003@gmail.com>
2025-06-26 19:10:03 -0500

    Cambio de tipado en game para el GameDTO

--- src/app/models/Game.ts
+++ src/app/models/Game.ts
@@ -1,12 +1,11 @@
-import { Category } from './Category';
-
 export class Game {
-  id!: number;
   name!: string;
   description!: string;
   release_date!: string;
   developer!: string;
-  game_minicard!: string;
-  game_wallpaper!: string;
-  categories!: Category[];
+  wallpaper!: string;
+  card!: string;
+  banner!: string;
+  logo!: string;
+  categories!: string[];
 }
