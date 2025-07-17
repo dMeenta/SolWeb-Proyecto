e8784731efe638fd963b690e883b4896463f1694
Diego Quipuzco <demential2003@gmail.com>
2025-07-17 16:03:34 -0500

    Loader while requesting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxSonnerToaster } from 'ngx-sonner';
+import { LoaderSpinnerComponent } from './components/loader-spinner/loader-spinner.component';
+import { NgIf } from '@angular/common';
+import { LoadingService } from './services/loading.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgxSonnerToaster],
+  imports: [RouterOutlet, NgxSonnerToaster, LoaderSpinnerComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = '';
+
+  constructor(public loadingService: LoadingService) {}
 }
--- src/app/shared/auth-guard.service.ts
+++ src/app/shared/auth-guard.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map, catchError, of } from 'rxjs';
+import { Observable, map, catchError, of, finalize } from 'rxjs';
 import { getApiUrl, apiConf } from '../config/apiConfig';
+import { LoadingService } from '../services/loading.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuardService {
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private loadingService: LoadingService
+  ) {}
 
   redirectIfNotLogged(): Observable<boolean> {
+    this.loadingService.show();
     return this.http
       .get(getApiUrl(apiConf.endpoints.user.getCurrentUserLogged()), {
         withCredentials: true,
@@ -20,7 +26,8 @@ export class AuthGuardService {
         catchError(() => {
           this.router.navigateByUrl('/auth/sign-in');
           return of(false);
-        })
+        }),
+        finalize(() => this.loadingService.hide())
       );
   }
 
